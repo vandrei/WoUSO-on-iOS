972560faec2ea4ae33223e4b919c59b1762411e6
Andrei Vasilescu <andrei.vasilescu@cti.pub.ro>
2014-01-15 00:04:24 +0200

    first commit

---
+++ WoUSO/WoUSO/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WoUSO
+//
+//  Created by Andrei Vasilescu on 15/01/14.
+//  Copyright (c) 2014 Rosedu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ WoUSO/WoUSOTests/WoUSOTests.m
@@ -0,0 +1,34 @@
+//
+//  WoUSOTests.m
+//  WoUSOTests
+//
+//  Created by Andrei Vasilescu on 15/01/14.
+//  Copyright (c) 2014 Rosedu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WoUSOTests : XCTestCase
+
+@end
+
+@implementation WoUSOTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
